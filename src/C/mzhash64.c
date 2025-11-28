db183775c82892efb9bfdbf40facb190b128b7e4
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2025-11-28 17:44:17 +0100

    Add files via upload

--- src/C/mzhash64.c
+++ src/C/mzhash64.c
@@ -1,47 +1,60 @@
 /**
- * mzHash64
+ * mzhash64
  * 
- * Strong, fast, simple, non-cryptographic 64-bit hash function
+ * Strong, fast, simple, non-cryptographic 32-bit hash function
  * 
  * Author: Matteo Zapparoli
- * Date: 2022
- * Licence: Public Domain
- * 
- * This is free and unencumbered software released into the public domain.
+ * Date: 2025-11
+ * Reelease: 3
  *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- * 
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * 
- * For more information, please refer to <https://unlicense.org>
+ * SPDX-FileCopyrightText: 2025 Matteo Zapparoli <zapparoli.matteo@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE file in project root.
  * 
  */
- 
+
 #include "mzhash64.h"
 
-uint64_t mzhash64(const char* data, size_t length, uint64_t seed)
+uint64_t mzhash64(const void* data, size_t length, uint64_t seed)
 {
-	uint64_t hash = 0xE297DA430DB2DF1ALL ^ seed;
+	const int8_t *bytes = (const int8_t*)data;
+	const int8_t *end = bytes + length;
+	uint64_t hash = 0xA7BB53D6328B05DBuLL ^ seed;
 
-	for (size_t i = 0; i < length; i++)
-		hash = 0xB2DEEF07CB4ACD43LL * (i + data[i]) ^ (hash << 2) ^ (hash >> 2);
+	while(bytes != end)
+		hash = 0xCAC39506BB87F535uLL * (*bytes++ ^ hash ^ (hash << 2) ^ (hash >> 2));
+
+	return hash;
+}
+
+uint64_t mzhash64_noseed(const void* data, size_t length)
+{
+	const int8_t *bytes = (const int8_t*)data;
+	const int8_t *end = bytes + length;
+	uint64_t hash = 0xA7BB53D6328B05DBuLL;
+
+	while(bytes != end)
+		hash = 0xCAC39506BB87F535uLL * (*bytes++ ^ hash ^ (hash << 2) ^ (hash >> 2));
+
+	return hash;
+}
+
+uint64_t mzhash64_str(const char* str, uint64_t seed)
+{
+	uint64_t hash = 0xA7BB53D6328B05DBuLL ^ seed;
+
+	while(*str)
+		hash = 0xCAC39506BB87F535uLL * (*str++ ^ hash ^ (hash << 2) ^ (hash >> 2));
+
+	return hash;
+}
+
+uint64_t mzhash64_str_noseed(const char* str)
+{
+	uint64_t hash = 0xA7BB53D6328B05DBuLL;
+
+	while(*str)
+		hash = 0xCAC39506BB87F535uLL * (*str++ ^ hash ^ (hash << 2) ^ (hash >> 2));
 
 	return hash;
 }
--- src/C/mzhash64.h
+++ src/C/mzhash64.h
@@ -1,39 +1,3 @@
-/**
- * mzHash64
- * 
- * Strong, fast, simple, non-cryptographic 64-bit hash function
- * 
- * Author: Matteo Zapparoli
- * Date: 2022
- * Licence: Public Domain
- * 
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- * 
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * 
- * For more information, please refer to <https://unlicense.org>
- * 
- */
-
 #ifndef _MZHASH64_H
 #define _MZHASH64_H
 
@@ -46,7 +10,10 @@ extern "C" {
 
 #define MZ_HASH_64_DEFAULT_SEED		0;
 
-uint64_t mzhash64(const char* data, size_t length, uint64_t seed);
+uint64_t mzhash64(const void* data, size_t length, uint64_t seed);
+uint64_t mzhash64_noseed(const void* data, size_t length);
+uint64_t mzhash64_str(const char* str, uint64_t seed);
+uint64_t mzhash64_str_noseed(const char* str);
 
 #ifdef __cplusplus
 }
