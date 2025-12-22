e37f80cac84f233e8dfe03dab232ed8718ff15cf
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2025-12-22 17:11:17 +0100

    Update mzhash64.c

--- src/C/mzhash64.c
+++ src/C/mzhash64.c
@@ -4,8 +4,8 @@
  * Strong, fast, simple, non-cryptographic 32-bit hash function
  * 
  * Author: Matteo Zapparoli
- * Date: 2025-11
- * Reelease: 3
+ * Date: 2025-12
+ * Reelease: 4
  *
  * SPDX-FileCopyrightText: 2025 Matteo Zapparoli <zapparoli.matteo@gmail.com>
  * SPDX-License-Identifier: Apache-2.0
@@ -19,10 +19,10 @@ uint64_t mzhash64(const void* data, size_t length, uint64_t seed)
 {
 	const int8_t *bytes = (const int8_t*)data;
 	const int8_t *end = bytes + length;
-	uint64_t hash = 0xA7BB53D6328B05DBuLL ^ seed;
+	uint64_t hash = 0xDED46DB8C47B7480uLL ^ seed;
 
 	while(bytes != end)
-		hash = 0xCAC39506BB87F535uLL * (*bytes++ ^ hash ^ (hash << 2) ^ (hash >> 2));
+		hash = 0xE958AC98E3D243C3uLL * (*bytes++ ^ hash ^ (hash << 2) ^ (hash >> 2));
 
 	return hash;
 }
@@ -31,30 +31,30 @@ uint64_t mzhash64_noseed(const void* data, size_t length)
 {
 	const int8_t *bytes = (const int8_t*)data;
 	const int8_t *end = bytes + length;
-	uint64_t hash = 0xA7BB53D6328B05DBuLL;
+	uint64_t hash = 0xDED46DB8C47B7480uLL;
 
 	while(bytes != end)
-		hash = 0xCAC39506BB87F535uLL * (*bytes++ ^ hash ^ (hash << 2) ^ (hash >> 2));
+		hash = 0xE958AC98E3D243C3uLL * (*bytes++ ^ hash ^ (hash << 2) ^ (hash >> 2));
 
 	return hash;
 }
 
 uint64_t mzhash64_str(const char* str, uint64_t seed)
 {
-	uint64_t hash = 0xA7BB53D6328B05DBuLL ^ seed;
+	uint64_t hash = 0xDED46DB8C47B7480uLL ^ seed;
 
 	while(*str)
-		hash = 0xCAC39506BB87F535uLL * (*str++ ^ hash ^ (hash << 2) ^ (hash >> 2));
+		hash = 0xE958AC98E3D243C3uLL * (*str++ ^ hash ^ (hash << 2) ^ (hash >> 2));
 
 	return hash;
 }
 
 uint64_t mzhash64_str_noseed(const char* str)
 {
-	uint64_t hash = 0xA7BB53D6328B05DBuLL;
+	uint64_t hash = 0xDED46DB8C47B7480uLL;
 
 	while(*str)
-		hash = 0xCAC39506BB87F535uLL * (*str++ ^ hash ^ (hash << 2) ^ (hash >> 2));
+		hash = 0xE958AC98E3D243C3uLL * (*str++ ^ hash ^ (hash << 2) ^ (hash >> 2));
 
 	return hash;
 }
